5bc304dcbc63b5680682ae16648e5f0534d3c0d5
kushalAgrawal17 <93080751+kushalAgrawal17@users.noreply.github.com>
2021-11-10 23:41:26 +0530

    Update cps_tb.v

--- cps_tb.v
+++ cps_tb.v
@@ -1,25 +1,25 @@
-/*`timescale 1ns / 1ps
+// `timescale 1ns / 1ps
 
-module car_parking_system_tb();
+// module car_parking_system_tb();
 
-    reg rClk;
-    reg rRst;
-    reg rsensorEntrance;
-    reg rsensorExit;
-    reg [3:0] rpasswordEntered;
-    wire wgreenLed, wredLed;
-    wire [6:0] wdisplay;
-    wire [3:0] wanodeActivate;
+//     reg rClk;
+//     reg rRst;
+//     reg rsensorEntrance;
+//     reg rsensorExit;
+//     reg [3:0] rpasswordEntered;
+//     wire wgreenLed, wredLed;
+//     wire [6:0] wdisplay;
+//     wire [3:0] wanodeActivate;
  
 
-    car_parking_system CPS_INST(.Clk(rClk), .Rst(rRst), .sensorEntrance(rsensorEntrance), .sensorExit(rsensorExit), .passwordEntered(rpasswordEntered), .greenLed(wgreenLed), .redLed(wredLed), .display(wdisplay), .anodeActivate(wanodeActivate));
+//     car_parking_system CPS_INST(.Clk(rClk), .Rst(rRst), .sensorEntrance(rsensorEntrance), .sensorExit(rsensorExit), .passwordEntered(rpasswordEntered), .greenLed(wgreenLed), .redLed(wredLed), .display(wdisplay), .anodeActivate(wanodeActivate));
 
-    initial begin
-    rClk = 0;
-    forever #10 rClk = ~rClk;
-    end
+//     initial begin
+//     rClk = 0;
+//     forever #10 rClk = ~rClk;
+//     end
  	 
-endmodule*/
+// endmodule
 
 `timescale 1ns / 1ps
 module car_parking_system_tb();
