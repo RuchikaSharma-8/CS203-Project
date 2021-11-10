215a56c7fe28f0a1bf9f24991f255f6053069ab9
RuchikaSharma-8 <93065377+RuchikaSharma-8@users.noreply.github.com>
2021-11-10 23:40:26 +0530

    Update cps_tb.v

--- cps_tb.v
+++ cps_tb.v
@@ -24,37 +24,38 @@ endmodule*/
 `timescale 1ns / 1ps
 module car_parking_system_tb();
 
-  // Inputs
-  reg rClk;
-  reg rRst;
-  reg rsensorEntrance;
-  reg rsensorExit;
-  reg [3:0] rpasswordEntered;
-  wire wgreenLed, wredLed;
-  wire [6:0] wdisplay;
-  wire [3:0] wanodeActivate;
- 
- car_parking_system CPS_INST(.Clk(rClk), .Rst(rRst), .sensorEntrance(rsensorEntrance), .sensorExit(rsensorExit), .passwordEntered(rpasswordEntered), .greenLed(wgreenLed), .redLed(wredLed), .display(wdisplay), .anodeActivate(wanodeActivate));
+      // Inputs
+      reg rClk;
+      reg rRst;
+      reg rsensorEntrance;
+      reg rsensorExit;
+      reg [3:0] rpasswordEntered;
+      wire wgreenLed, wredLed;
+      wire [6:0] wdisplay;
+      wire [3:0] wanodeActivate;
 
- initial begin
- rClk = 0;
- forever #10 rClk = ~rClk;
- end
-initial begin
- // Initialize Inputs
- rRst = 0;
- rsensorEntrance = 0;
- rsensorExit = 0;
- rpasswordEntered = 0;
- // Wait 100 ns for global reset to finish
- #100;
-      rRst = 1;
- #20;
- rsensorEntrance = 1;
- #1000;
- rsensorEntrance = 0;
- rpasswordEntered = 1;
- #2000;
- rsensorExit =1;
- end
+     car_parking_system CPS_INST(.Clk(rClk), .Rst(rRst), .sensorEntrance(rsensorEntrance), .sensorExit(rsensorExit), .passwordEntered(rpasswordEntered), .greenLed(wgreenLed), .redLed(wredLed), .display(wdisplay), .anodeActivate(wanodeActivate));
+
+     initial begin
+     rClk = 0;
+     forever #10 rClk = ~rClk;
+     end
+    initial begin
+     // Initialize Inputs
+     rRst = 0;
+     rsensorEntrance = 0;
+     rsensorExit = 0;
+     rpasswordEntered = 0;
+     // Wait 100 ns for global reset to finish
+     #100;
+          rRst = 1;
+     #20;
+     rsensorEntrance = 1;
+     #1000;
+     rsensorEntrance = 0;
+     rpasswordEntered = 1;
+     #2000;
+     rsensorExit =1;
+     end
+    
  endmodule
