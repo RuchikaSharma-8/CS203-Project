932b4c2aa5c2fcdf210ecb61a9bf31c92a46995c
kushalAgrawal17 <93080751+kushalAgrawal17@users.noreply.github.com>
2021-11-14 15:01:36 +0530

    Update cps_tb.v

--- cps_tb.v
+++ cps_tb.v
@@ -1,26 +1,3 @@
-// `timescale 1ns / 1ps
-
-// module car_parking_system_tb();
-
-//     reg rClk;
-//     reg rRst;
-//     reg rsensorEntrance;
-//     reg rsensorExit;
-//     reg [3:0] rpasswordEntered;
-//     wire wgreenLed, wredLed;
-//     wire [6:0] wdisplay;
-//     wire [3:0] wanodeActivate;
- 
-
-//     car_parking_system CPS_INST(.Clk(rClk), .Rst(rRst), .sensorEntrance(rsensorEntrance), .sensorExit(rsensorExit), .passwordEntered(rpasswordEntered), .greenLed(wgreenLed), .redLed(wredLed), .display(wdisplay), .anodeActivate(wanodeActivate));
-
-//     initial begin
-//     rClk = 0;
-//     forever #10 rClk = ~rClk;
-//     end
- 	 
-// endmodule
-
 `timescale 1ns / 1ps
 module car_parking_system_tb();
 
@@ -33,8 +10,10 @@ module car_parking_system_tb();
       wire wgreenLed, wredLed;
       wire [6:0] wdisplay;
       wire [3:0] wanodeActivate;
+    wire [3:0] wpass, wpass2;
+    wire wsenEnt, wsenEx;
 
-     car_parking_system CPS_INST(.Clk(rClk), .Rst(rRst), .sensorEntrance(rsensorEntrance), .sensorExit(rsensorExit), .passwordEntered(rpasswordEntered), .greenLed(wgreenLed), .redLed(wredLed), .display(wdisplay), .anodeActivate(wanodeActivate));
+     car_parking_system CPS_INST(.Clk(rClk), .Rst(rRst), .sensorEntrance(rsensorEntrance), .sensorExit(rsensorExit), .passwordEntered(rpasswordEntered), .greenLed(wgreenLed), .redLed(wredLed), .display(wdisplay), .anodeActivate(wanodeActivate), .pass(wpass), .pass2(wpass2), .senEnt(wsenEnt), .senEx(wsenEx));
 
      initial begin
      rClk = 0;
@@ -57,5 +36,4 @@ module car_parking_system_tb();
      #2000;
      rsensorExit =1;
      end
-    
- endmodule
+
